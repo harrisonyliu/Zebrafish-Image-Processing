96696bd0ff3b25c90de675d495142c58be2d809d
harrisonyliu <harrisonyliu@gmail.com>
2015-05-04 10:48:14 -0700

    Edited braincheck
    When a file is backed up a .db file is left that messes up the old
    version.

--- brainCheck.m
+++ brainCheck.m
@@ -121,12 +121,17 @@ function next_image(hObject, eventdata, handles)
 if handles.curr_im <= numel(handles.file_dir)-1
     handles.curr_im = handles.curr_im + 1;
     handles.curr_fname = fullfile(handles.pathname, handles.file_dir(handles.curr_im).name);
-    if strfind(handles.curr_fname, 'Thumbs.db') == 0
+    if isempty(strfind(handles.curr_fname, 'Thumbs.db')) == 1
         temp_im = imread(handles.curr_fname);
         imagesc(temp_im,'Parent',handles.axes1);colormap gray;axis image;axis off;
         [wellname, ~, ~] = extract_wellname(handles.curr_fname);
         title(wellname);
         guidata(hObject, handles);
+    else
+        msgbox('You have finished the data set!');
+        handles.output = handles.err_matrix;
+        guidata(hObject,handles);
+        uiresume(handles.figure1);
     end
 else
     msgbox('You have finished the data set!');
